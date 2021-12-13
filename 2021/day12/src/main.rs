2659e04f5fc44a7d1f52d17fd0c5e3ee0eabada7
hunomina <hugo.f972@gmail.com>
2021-12-13 21:47:36 +0100

    Add 2021/12

---
+++ 2021/day12/src/main.rs
@@ -0,0 +1,126 @@
+use std::{collections::HashMap, fs};
+
+const START: &str = "start";
+const END: &str = "end";
+
+fn main() {
+    let input = fs::read_to_string("src/input.txt").unwrap();
+    let caves = parse_input(&input);
+
+    println!("part1 result {}", part1(caves.clone()));
+    println!("part2 result {}", part2(caves.clone()));
+}
+
+fn parse_input(input: &str) -> HashMap<&str, Vec<&str>> {
+    let mut caves: HashMap<&str, Vec<&str>> = HashMap::new();
+
+    input.lines().for_each(|line| {
+        let mut splitted_line = line.split('-');
+        let (from, to) = (splitted_line.nth(0).unwrap(), splitted_line.nth(0).unwrap());
+
+        if caves.contains_key(&from) {
+            caves.get_mut(&from).unwrap().push(to.clone());
+        } else {
+            caves.insert(from.clone(), vec![to.clone()]);
+        }
+
+        if caves.contains_key(&to) {
+            caves.get_mut(&to).unwrap().push(from.clone());
+        } else {
+            caves.insert(to.clone(), vec![from.clone()]);
+        }
+    });
+
+    caves
+}
+
+fn part1(caves: HashMap<&str, Vec<&str>>) -> usize {
+    get_paths_to_end_part1(&caves, vec![START]).len()
+}
+
+fn part2(caves: HashMap<&str, Vec<&str>>) -> usize {
+    get_paths_to_end_part2(&caves, vec![START]).len()
+}
+
+fn get_paths_to_end_part1<'a>(
+    caves: &'a HashMap<&str, Vec<&str>>,
+    from: Vec<&'a str>,
+) -> Vec<Vec<&'a str>> {
+    caves
+        .get(from.last().unwrap())
+        .unwrap()
+        .into_iter()
+        .filter_map(|linked_cave| {
+            if is_small_cave(linked_cave) && from.contains(linked_cave) {
+                None
+            } else if *linked_cave == END {
+                let mut local_from = from.clone();
+                local_from.push(linked_cave);
+                Some(vec![local_from])
+            } else {
+                let mut local_from = from.clone();
+                local_from.push(linked_cave);
+                Some(get_paths_to_end_part1(&caves, local_from))
+            }
+        })
+        .flatten()
+        .collect()
+}
+
+fn get_paths_to_end_part2<'a>(
+    caves: &'a HashMap<&str, Vec<&str>>,
+    from: Vec<&'a str>,
+) -> Vec<Vec<&'a str>> {
+    caves
+        .get(from.last().unwrap())
+        .unwrap()
+        .into_iter()
+        .filter_map(|linked_cave| {
+            if *linked_cave == END {
+                let mut local_from = from.clone();
+                local_from.push(linked_cave);
+                Some(vec![local_from])
+            } else if can_visit_cave(&from, linked_cave) {
+                let mut local_from = from.clone();
+                local_from.push(linked_cave);
+                Some(get_paths_to_end_part2(&caves, local_from))
+            } else {
+                None
+            }
+        })
+        .flatten()
+        .collect()
+}
+
+fn is_small_cave(cave: &str) -> bool {
+    cave.chars().nth(0).unwrap().is_lowercase()
+}
+
+fn can_visit_cave<'a>(history: &Vec<&str>, cave: &str) -> bool {
+    if cave == START || cave == END {
+        false
+    } else if is_small_cave(cave) {
+        count_cave_occurence(&history, cave) == 0
+            || count_max_occurence_of_small_caves(&history) < 2
+    } else {
+        true
+    }
+}
+
+fn count_cave_occurence(history: &Vec<&str>, cave: &str) -> usize {
+    history.iter().fold(0, |acc, c| acc + (*c == cave) as usize)
+}
+
+fn count_max_occurence_of_small_caves(history: &Vec<&str>) -> usize {
+    history
+        .iter()
+        .filter(|cave| is_small_cave(cave))
+        .fold(HashMap::<&str, usize>::new(), |mut m, x| {
+            *m.entry(x).or_default() += 1;
+            m
+        })
+        .into_iter()
+        .max_by_key(|(_, v)| *v)
+        .map(|(_, v)| v)
+        .unwrap()
+}
