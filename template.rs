489defd7b84f16957621fa4963e205887e1bce54
Hugo FOUGERES <hugo.fougeres@angi.com>
2025-12-06 16:46:13 +0100

    Update template

--- template.rs
+++ template.rs
@@ -3,6 +3,9 @@ use std::fs;
 fn main() {
     let input = fs::read_to_string("src/input.txt").unwrap();
     let input = parse_input(input.as_str());
+
+    println!("part1 {:?}", part1(input.clone()));
+    println!("part2 {:?}", part2(input.clone()));
 }
 
 type Input = ();
